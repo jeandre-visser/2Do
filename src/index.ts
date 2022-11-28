2a653226683773298f366c1967065e737cd24176
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-28 15:47:10 -0700

    Create Task type and add to function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { v4 as uuidV4 } from "uuid";
 
+// type props for Task
+type Task = { 
+  id: string, 
+  title: string, 
+  completed: boolean, 
+  createdAt: Date 
+}
+
 const list = document.querySelector<HTMLUListElement>("#list");
 const form = document.querySelector<HTMLFormElement>("#task-form");
 const input = document.querySelector<HTMLInputElement>("#task-input");
@@ -10,10 +18,16 @@ form?.addEventListener("submit", (e) => {
   // Check if input is empty or null, if so, then return since user has not input any task yet
   if (input?.value == "" || input?.value == null) return;
   
-  const task = {
+  const newTask: Task = {
     id: uuidV4(),
     title: input.value,
     completed: false,
     createdAt: new Date()
   }
+
+  addListItem(newTask);
 })
+
+const addListItem = (task: Task) => {
+  
+}
